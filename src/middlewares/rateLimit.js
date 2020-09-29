db3cad8b79112c64213cfc082960fd5c614eef13
Richard Henrique <richard.passos@beetech.global>
2020-09-28 21:38:59 -0300

    Update rateLimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const expressRateLimit = require('express-rate-limit');
 
 const rateLimit = expressRateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 500,
   message: 'Too many requests baby doll'
 });
 
